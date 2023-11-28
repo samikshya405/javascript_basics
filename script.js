c1fcc3a1f348af3eeac6da9afbd2ff4ad60ab7b6
Samikshya Kharel <samikshyakharel@Samikshyas-MacBook-Air.local>
2023-11-28 18:12:32 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+// const product =['laptop','Smartphone','Headphones', 'keyboard', 'Mouse']
+// const price = [1000,500,100,50,20]
+// let total = 0;
+// console.log('Available Products--------')    
+// let chose ='y'
+// let added = []
+// //let chose = prompt('do you want to add more item');
+// // if(chose ==='y' || chose ==='Y'){
+// //     console.log('ok');
+// // }
+// while(chose==='y'){
+//     for(let i =0;i<product.length;i++){
+
+//         console.log((i+1) + '. ' + product[i] + ': ' + price[i])
+//     }
+//     let choose = prompt('choose number');
+//     added.push(choose);
+//     console.log(added);
+//     let choose_numeric = Number(choose);
+//     total = total + price[choose_numeric-1];
+//     console.log('total amount to pay', total);
+//     chose = prompt('do you wish to comtine');
+// }
+// console.log('your cart');
+
+// for(let i =0;i<added.length;i++){
+
+//     console.log((i+1) + '. ' + product[added[i]-1] + ': ' + price[added[i]-1])
+// }
+// console.log('total amount to pay', total);
+
